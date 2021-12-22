08de82e77c5f0b04cb463d7d99e5efde6e42d1cc
lyj00912 <3122742760@qq.com>
2021-12-23 00:42:20 +0800

    worked out the problem by pixel operations, but too slow to run with python, it took 20-30 second to process a single image, maybe I should rewrite it using C++

--- main.py
+++ main.py
@@ -15,25 +15,30 @@ if __name__ == "__main__":
         if imageChannelCount == 3:
             # JPG format, only have 3 channels
             # Then convert it into 4-channel PNG format
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
+        elif imageChannelCount == 4:
+            # image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGRA)
+            pass
+            pass
+        else:
+            raise BaseException("Unsupported Channel Number: " + str(imageChannelCount))
 
         # Assure that the image is in right format
         imageChannelCount = image.shape[2]
         assert imageChannelCount == 4
 
-        # Get the alpha channel of the image
-        alpha_channel = image[:, :, 3]
+        count = 0
+        for row in image:
+            count += 1
+            # print(count)
+            for pixel in row:
+                # print(pixel)
+                alpha = pixel[3] / 255
+                # print(alpha)
+                pixel[0] = pixel[0] * alpha + 255 * (1 - alpha)
+                pixel[1] = pixel[1] * alpha + 255 * (1 - alpha)
+                pixel[2] = pixel[2] * alpha + 255 * (1 - alpha)
 
-        # Get the mask
-        _, mask = cv2.threshold(alpha_channel, 254, 255, cv2.THRESH_BINARY)  # binarize mask
-
-        # Get the color channels of the image
-        color = image[:, :, :3]
-
-        # Mask it up
-        image = cv2.bitwise_not(cv2.bitwise_not(color, mask=mask))
-
-        cv2.imshow("Window", image)
-        cv2.waitKey(0)
-
-        # # cv2.imwrite(item[:-4] + ".jpg", image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+        cv2.imwrite(item[:-4] + ".jpg", image)
+        print("Finished processing: " + item[:-4] + ".jpg")
